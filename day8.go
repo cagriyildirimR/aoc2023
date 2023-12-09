b843c692484c6c3bb3fa1d2d3c3cdd5dc5cf4a3f
Cagri Yildirim <cagri.yildirim.r@gmail.com>
2023-12-09 22:55:31 +0300

    Refactor Day 8

--- day8.go
+++ day8.go
@@ -5,134 +5,96 @@ import (
 	"strings"
 )
 
-func Day8() {
-
-	_, input := Input(8)
-
-	ins := input[0]
-	var network []Node
-
-	for i := 2; i < len(input); i++ {
-		tmp := strings.Split(input[i], " = ")
-		n := Node{Head: tmp[0]}
-		lr := strings.Split(strings.Trim(tmp[1], "()"), ", ")
-		n.Left = lr[0]
-		n.Right = lr[1]
-		network = append(network, n)
-	}
-
-	var networkMap map[string]Node = make(map[string]Node)
-	for _, v := range network {
-		networkMap[v.Head] = v
-	}
-
-	//result, loop := findEnd(ins, networkMap)
-
-	var startNodes []string
-	for _, v := range network {
-		if v.Head[2] == 'A' {
-			startNodes = append(startNodes, v.Head)
-		}
-	}
-
-	fmt.Printf("Starting nodes are %q\n", startNodes)
-
-	var results []int64
-	for _, s := range startNodes {
-		y, _ := findEnd2(s, ins, networkMap)
-		results = append(results, y)
-	}
-
-	fmt.Println(lcmOfList(results))
-
+// Node represents a node in the network.
+type Node struct {
+	Head, Left, Right string
 }
 
-// gcd calculates the Greatest Common Divisor using the Euclidean algorithm
+// Day8 executes the main logic for Day 8 challenge.
+func Day8() {
+	_, input := Input(8)
+	instructions := input[0]
+	networkMap := buildNetworkMap(input[2:])
+
+	// Part One
+	stepsToZZZ, _ := navigateFromNode("AAA", instructions, networkMap)
+	fmt.Printf("Steps required to reach ZZZ (Part One): %d\n", stepsToZZZ)
+
+	// Part Two
+	startNodes := findStartingNodes(networkMap)
+	results := navigateNetwork(instructions, networkMap, startNodes)
+	fmt.Printf("Combined steps to reach nodes ending with Z (Part Two): %d\n", lcmOfList(results))
+}
+
+// buildNetworkMap constructs a map of nodes from the input slice of strings.
+func buildNetworkMap(input []string) map[string]Node {
+	networkMap := make(map[string]Node)
+	for _, line := range input {
+		parts := strings.Split(line, " = ")
+		lr := strings.Split(strings.Trim(parts[1], "()"), ", ")
+		networkMap[parts[0]] = Node{Head: parts[0], Left: lr[0], Right: lr[1]}
+	}
+	return networkMap
+}
+
+// findStartingNodes identifies all nodes ending with 'A' to start the navigation.
+func findStartingNodes(networkMap map[string]Node) []string {
+	var startNodes []string
+	for _, node := range networkMap {
+		if node.Head[len(node.Head)-1] == 'A' {
+			startNodes = append(startNodes, node.Head)
+		}
+	}
+	return startNodes
+}
+
+// navigateNetwork processes each starting node and calculates the number of steps to reach the end.
+func navigateNetwork(ins string, networkMap map[string]Node, startNodes []string) []int64 {
+	var results []int64
+	for _, startNode := range startNodes {
+		steps, _ := navigateFromNode(startNode, ins, networkMap)
+		results = append(results, steps)
+	}
+	return results
+}
+
+// navigateFromNode navigates the network from a given node according to the instructions.
+func navigateFromNode(node string, ins string, networkMap map[string]Node) (int64, bool) {
+	var steps int64
+	for {
+		for _, direction := range ins {
+			currentNode := networkMap[node]
+			if direction == 'L' {
+				node = currentNode.Left
+			} else if direction == 'R' {
+				node = currentNode.Right
+			}
+			steps++
+			if node[len(node)-1] == 'Z' {
+				return steps, true
+			}
+		}
+	}
+}
+
+// gcd calculates the greatest common divisor of two integers.
 func gcd(a, b int64) int64 {
 	for b != 0 {
-		t := b
-		b = a % b
-		a = t
+		a, b = b, a%b
 	}
 	return a
 }
 
-// lcm calculates the Least Common Multiple of two integers
+// lcm calculates the least common multiple of two integers.
 func lcm(a, b int64) int64 {
 	return a * b / gcd(a, b)
 }
 
-// lcmOfList calculates the LCM of a slice of integers
+// lcmOfList calculates the least common multiple of a list of integers.
 func lcmOfList(arr []int64) int64 {
 	lcmValue := arr[0]
 	for _, num := range arr[1:] {
 		lcmValue = lcm(lcmValue, num)
 	}
 	return lcmValue
-}
-
-func findEnd(ins string, networkMap map[string]Node) (int64, bool) {
-	var result int64
-	pos := "AAA"
-	loop := true
-	for loop {
-		for _, v := range ins {
-			if v == 'L' {
-				pos = networkMap[pos].Left
-			}
-			if v == 'R' {
-				pos = networkMap[pos].Right
-			}
-			result++
-			if pos == "ZZZ" {
-				loop = false
-				break
-			}
-		}
-	}
-	fmt.Println(result)
-	return result, loop
-}
-
-func findEnd2(pos string, ins string, networkMap map[string]Node) (int64, bool) {
-	var result int64
-	loop := true
-	for loop {
-		for _, v := range ins {
-			if v == 'L' {
-				pos = networkMap[pos].Left
-			}
-			if v == 'R' {
-				pos = networkMap[pos].Right
-			}
-			result++
-			if pos[2] == 'Z' {
-				loop = false
-				break
-			}
-		}
-	}
-	fmt.Printf("pos: %q has end %v\n", pos, result)
-	return result, loop
-}
-
-type Node struct {
-	Head, Left, Right string
-}
-
-func (n Node) String() string {
-	return fmt.Sprintf("Head: %v, Left: %v, Right: %v", n.Head, n.Left, n.Right)
-}
-
-func allZ(ss []string, result int64) bool {
-	for i, s := range ss {
-		if s[2] != 'Z' {
-			return false
-		}
-
-		if s[2] == 'Z' {
-			fmt.Printf("index %v reached it's end in %v iterations\n", i, result)
-		}
-	}
-	return true
 }
