a57f04644aab2c33d121c87ca99beca5a4b1b348
Cagri Yildirim <cagri.yildirim.r@gmail.com>
2023-12-09 22:00:01 +0300

    Refactor Day 5

--- day5.go
+++ day5.go
@@ -1,9 +1,9 @@
 package adventOfCode23
 
 import (
+	"errors"
 	"fmt"
 	"log"
-	"math"
 	"strconv"
 	"strings"
 )
@@ -20,145 +20,157 @@ func (m *Mapper) String() string {
 
 type Seeds []int64
 
+type Range struct {
+	start, end int64
+	processed  bool
+}
+
 func Day5() {
 	_, input := Input(5)
 
-	m := make(map[string][]Mapper)
-	m["seed-to-soil map:"] = []Mapper{}
-	m["soil-to-fertilizer map:"] = []Mapper{}
-	m["fertilizer-to-water map:"] = []Mapper{}
-	m["water-to-light map:"] = []Mapper{}
-	m["light-to-temperature map:"] = []Mapper{}
-	m["temperature-to-humidity map:"] = []Mapper{}
-	m["humidity-to-location map:"] = []Mapper{}
-
-	var seeds Seeds
-	ss := strings.Fields(strings.Split(input[0], ":")[1])
-	for _, v := range ss {
-		w, _ := strconv.Atoi(v)
-		seeds = append(seeds, int64(w))
+	maps := []string{
+		"seed-to-soil map:", "soil-to-fertilizer map:", "fertilizer-to-water map:",
+		"water-to-light map:", "light-to-temperature map:", "temperature-to-humidity map:",
+		"humidity-to-location map:",
 	}
 
-	var key string
-	for _, v := range input[1:] {
+	m := make(map[string][]Mapper)
+	for _, key := range maps {
+		m[key] = []Mapper{}
+	}
 
-		switch v {
-		case "seed-to-soil map:":
-			key = "seed-to-soil map:"
-			continue
-		case "soil-to-fertilizer map:":
-			key = "soil-to-fertilizer map:"
-			continue
-		case "fertilizer-to-water map:":
-			key = "fertilizer-to-water map:"
-			continue
-		case "water-to-light map:":
-			key = "water-to-light map:"
-			continue
-		case "light-to-temperature map:":
-			key = "light-to-temperature map:"
-			continue
-		case "temperature-to-humidity map:":
-			key = "temperature-to-humidity map:"
-			continue
-		case "humidity-to-location map:":
-			key = "humidity-to-location map:"
-			continue
-		case "":
-			continue
-		default:
-			numbers := strings.Fields(v)
-			if len(numbers) < 3 {
-				continue
+	seeds, err := parseSeeds(strings.Split(input[0], ":")[1])
+	if err != nil {
+		log.Fatalf("Error parsing seeds: %v", err)
+	}
+
+	key := ""
+	for _, line := range input[1:] {
+		if contains(maps, line) {
+			key = line
+		} else {
+			mapper, err := parseMapper(line)
+			if err == nil {
+				m[key] = append(m[key], mapper)
 			}
-			destination, err1 := strconv.ParseInt(numbers[0], 10, 64)
-			source, err2 := strconv.ParseInt(numbers[1], 10, 64)
-			rang, err3 := strconv.ParseInt(numbers[2], 10, 64)
-
-			if err1 != nil || err2 != nil || err3 != nil {
-				continue
-			}
-
-			mapper := Mapper{Destination: destination, Source: source, Rang: rang}
-			m[key] = append(m[key], mapper)
 		}
 	}
 
-	lo := []string{"seed-to-soil map:", "soil-to-fertilizer map:", "fertilizer-to-water map:", "water-to-light map:", "light-to-temperature map:", "temperature-to-humidity map:", "humidity-to-location map:"}
-	for _, v := range lo { // loop through map
-		for i, s := range seeds {
+	processSeedsThroughLayers(&seeds, m, maps)
+
+	rangeSeeds := createRangeSeeds(seeds)
+	processRangeSeedsThroughLayers(&rangeSeeds, m, maps)
+
+	minEndValue := findMinEndValue(rangeSeeds)
+	fmt.Printf("Result: %d\n", minimum(seeds))
+	fmt.Printf("Minimum end value: %d\n", minEndValue.start)
+}
+
+func contains(slice []string, item string) bool {
+	for _, v := range slice {
+		if v == item {
+			return true
+		}
+	}
+	return false
+}
+
+func parseSeeds(input string) (Seeds, error) {
+	var seeds Seeds
+	ss := strings.Fields(input)
+	for _, v := range ss {
+		w, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		seeds = append(seeds, w)
+	}
+	return seeds, nil
+}
+
+func parseMapper(input string) (Mapper, error) {
+	numbers := strings.Fields(input)
+	if len(numbers) < 3 {
+		return Mapper{}, errors.New("invalid mapper format")
+	}
+
+	destination, err1 := strconv.ParseInt(numbers[0], 10, 64)
+	source, err2 := strconv.ParseInt(numbers[1], 10, 64)
+	rang, err3 := strconv.ParseInt(numbers[2], 10, 64)
+	if err1 != nil || err2 != nil || err3 != nil {
+		return Mapper{}, fmt.Errorf("error parsing mapper: %v, %v, %v", err1, err2, err3)
+	}
+
+	return Mapper{Destination: destination, Source: source, Rang: rang}, nil
+}
+
+func processSeedsThroughLayers(seeds *Seeds, m map[string][]Mapper, maps []string) {
+	for _, v := range maps {
+		for i, s := range *seeds {
 			for _, w := range m[v] {
-				b, l := isIn(s, w)
-				if b {
-					seeds[i] = l
+				if b, l := isIn(s, w); b {
+					(*seeds)[i] = l
 					break
 				}
 			}
 		}
 	}
+}
 
-	result := seeds[0]
-	for _, s := range seeds {
-		//fmt.Println(s)
-		if result > s {
-			result = s
+func minimum(seeds Seeds) int64 {
+	m := seeds[0]
+	for _, s := range seeds[1:] {
+		if s < m {
+			m = s
 		}
 	}
+	return m
+}
 
-	fmt.Println(result)
-
-	seeds = Seeds{}
-	for _, v := range ss {
-		w, _ := strconv.Atoi(v)
-		seeds = append(seeds, int64(w))
-	}
-
+func createRangeSeeds(seeds Seeds) []Range {
 	var rangeSeeds []Range
-	log.Println("Initializing rangeSeeds")
-	for i := 0; i < len(seeds); i = i + 2 {
+	for i := 0; i < len(seeds); i += 2 {
 		rangeSeeds = append(rangeSeeds, Range{
 			start:     seeds[i],
 			end:       seeds[i] + seeds[i+1],
 			processed: false,
 		})
 	}
+	return rangeSeeds
+}
 
-	for _, v := range lo { // layers
-		for i := 0; i < len(rangeSeeds); i++ { // take seed
-			s := rangeSeeds[i]
-			for _, w := range m[v] { // ranges inside layers
+func processRangeSeedsThroughLayers(rangeSeeds *[]Range, m map[string][]Mapper, maps []string) {
+	for _, v := range maps {
+		for i := 0; i < len(*rangeSeeds); i++ {
+			s := (*rangeSeeds)[i]
+			for _, w := range m[v] {
 				rs := rangeMap(s, w)
 				if len(rs) != 0 {
 					for _, r := range rs {
 						if r.processed {
-							rangeSeeds[i] = r
+							(*rangeSeeds)[i] = r
 						} else {
-							rangeSeeds = append(rangeSeeds, r)
+							*rangeSeeds = append(*rangeSeeds, r)
 						}
 					}
 					break
 				}
 			}
 		}
-		for i := range rangeSeeds {
-			rangeSeeds[i].processed = false
+		for i := range *rangeSeeds {
+			(*rangeSeeds)[i].processed = false
 		}
 	}
+}
 
-	log.Println("Calculating minimum end value")
-	mx := Range{
-		start:     math.MaxInt64,
-		end:       math.MaxInt64,
-		processed: false,
-	}
+func findMinEndValue(rangeSeeds []Range) Range {
+	minEndValue := rangeSeeds[0]
 	for _, r := range rangeSeeds {
-		if r.start < mx.start {
-			log.Printf("New minimum end value found: %d\n", r.end)
-			mx = r
+		if r.start < minEndValue.start {
+			minEndValue = r
 		}
 	}
-	fmt.Println("Minimum end value:", mx.start)
-
+	return minEndValue
 }
 
 func isIn(seed int64, m Mapper) (bool, int64) {
@@ -168,57 +180,57 @@ func isIn(seed int64, m Mapper) (bool, int64) {
 	return false, seed
 }
 
-type Range struct {
-	start, end int64
-	processed  bool
-}
-
 func rangeMap(sourceRange Range, mapper Mapper) []Range {
 	var result []Range
 	offset := mapper.Destination - mapper.Source
 
-	if sourceRange.start <= mapper.Source && sourceRange.end > mapper.Source && sourceRange.end < mapper.Source+mapper.Rang {
+	if isOverlapInFirstScenario(sourceRange, mapper) {
 		result = append(result, Range{mapper.Source + offset, sourceRange.end + offset, true})
 		if sourceRange.start != mapper.Source {
 			result = append(result, Range{sourceRange.start, mapper.Source, false})
 		}
-
 		return result
 	}
 
-	if sourceRange.start > mapper.Source && sourceRange.start < mapper.Source+mapper.Rang && sourceRange.end >= mapper.Source+mapper.Rang {
+	if isOverlapInSecondScenario(sourceRange, mapper) {
 		result = append(result, Range{sourceRange.start + offset, mapper.Source + mapper.Rang + offset, true})
-
 		if sourceRange.end != mapper.Source+mapper.Rang {
 			result = append(result, Range{mapper.Source + mapper.Rang, sourceRange.end, false})
 		}
-
 		return result
 	}
 
-	if sourceRange.end < mapper.Source {
-		return result
-	}
-
-	if sourceRange.start > mapper.Source+mapper.Rang {
-		return result
-	}
-
-	if sourceRange.start <= mapper.Source && sourceRange.end >= mapper.Source+mapper.Rang {
+	if isCompleteOverlap(sourceRange, mapper) {
 		result = append(result, Range{mapper.Source + offset, mapper.Source + mapper.Rang + offset, true})
 		if sourceRange.start != mapper.Source {
 			result = append(result, Range{sourceRange.start, mapper.Source, false})
 		}
-
 		if sourceRange.end != mapper.Source+mapper.Rang {
 			result = append(result, Range{mapper.Source + mapper.Rang, sourceRange.end, false})
 		}
+		return result
 	}
 
-	if sourceRange.start >= mapper.Source && sourceRange.end <= mapper.Source+mapper.Rang {
+	if isContainedWithin(sourceRange, mapper) {
 		result = append(result, Range{sourceRange.start + offset, sourceRange.end + offset, true})
 		return result
 	}
 
 	return result
+}
+
+func isOverlapInFirstScenario(sourceRange Range, mapper Mapper) bool {
+	return sourceRange.start <= mapper.Source && sourceRange.end > mapper.Source && sourceRange.end < mapper.Source+mapper.Rang
+}
+
+func isOverlapInSecondScenario(sourceRange Range, mapper Mapper) bool {
+	return sourceRange.start > mapper.Source && sourceRange.start < mapper.Source+mapper.Rang && sourceRange.end >= mapper.Source+mapper.Rang
+}
+
+func isCompleteOverlap(sourceRange Range, mapper Mapper) bool {
+	return sourceRange.start <= mapper.Source && sourceRange.end >= mapper.Source+mapper.Rang
+}
+
+func isContainedWithin(sourceRange Range, mapper Mapper) bool {
+	return sourceRange.start >= mapper.Source && sourceRange.end <= mapper.Source+mapper.Rang
 }
